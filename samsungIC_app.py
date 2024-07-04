4c07e1383b289816c7e4a8225d67c845374f2f50
Türkan Rişvan <turkanrisvan887@gmail.com>
2024-07-04 15:16:19 +0300

    commit türkan

--- samsungIC_app.py
+++ samsungIC_app.py
@@ -8,7 +8,6 @@ import time
 
 from streamlit_autorefresh import st_autorefresh
 
-
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: "%.3f" % x)
